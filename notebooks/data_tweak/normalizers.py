0b5542fb2a80d999777f323aeed4a4fe952d310c
kroy <kroy.xc@gmail.com>
2022-06-15 16:05:20 -0400

    feat: rebuild our profiles based on stops file; pretty decent identification of officers in stop file

--- notebooks/data_tweak/normalizers.py
+++ notebooks/data_tweak/normalizers.py
@@ -9,7 +9,7 @@ def normalize_sex(sex: str) -> str:
 def normalize_race(race: str) -> str:
   if (race in ["WHITE", "BLACK", "HISPANIC", "ASIAN/PACIFIC ISLANDER", "NATIVE AMERICAN/ALASKAN NATIVE"]):
     return race
-  elif race == "WHITE HISPANIC":
+  elif race in ["WHITE HISPANIC", "SPANISH (DO NOT USE)"]:
     return "HISPANIC"
   elif race in ["AMER IND/ALASKAN NATIVE", "NATIVE AMERICAN"]:
     return "NATIVE AMERICAN/ALASKAN NATIVE"
